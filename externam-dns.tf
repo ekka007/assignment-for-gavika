50ccb81e9e86f2f6a3c67b3b780407f9a419c593
SAROJ-DEVOPS <105065311+ekka007@users.noreply.github.com>
2022-11-28 16:52:34 +0530

    Create externam-dns.tf

---
+++ externam-dns.tf
@@ -0,0 +1,12 @@
+module "terraform-aws-eks-external-dns" {
+  count                   = var.option_external_dns_enabled ? 1 : 0
+  source                  = "./terraform-aws-eks-external-dns"
+  domain                  = gavika.dev
+  cluster_name            = local.cluster_name
+  cluster_region          = var.AWS_REGION
+  cluster_oidc_issuer_url = local.cluster_oidc_issuer_url
+  //  cluster_vpc_id = aws_vpc.cluster.id
+  common_tags       = local.common_tags
+  oidc_provider_arn = local.oidc_provider_arn
+  depends_on        = [aws_eks_cluster.eks-cluster]
+}
